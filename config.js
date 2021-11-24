c3773a26b2425177fb44019ea5cdcef2326ae868
Devanka <90453179+devanka761@users.noreply.github.com>
2021-11-24 15:13:46 +0700

    EPISODE 1

---
+++ config.js
@@ -0,0 +1,12 @@
+// REPLACE DENGAN CONFIG PROJECT KALIAN
+
+const firebaseConfig = {
+    apiKey: "GANTI_DENGAN_PUNYA_KALIAN",
+    authDomain: "GANTI_DENGAN_PUNYA_KALIAN",
+    databaseURL: "GANTI_DENGAN_PUNYA_KALIAN",
+    projectId: "GANTI_DENGAN_PUNYA_KALIAN",
+    storageBucket: "GANTI_DENGAN_PUNYA_KALIAN",
+    messagingSenderId: "GANTI_DENGAN_PUNYA_KALIAN",
+    appId: "GANTI_DENGAN_PUNYA_KALIAN"
+};
+const app = firebase.initializeApp(firebaseConfig);
